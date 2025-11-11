a2fe9c47bdb01c02f13abdb779a67a81e861963d
StevenGS4 <gistgallagamo@ittepic.edu.mx>
2025-11-11 16:31:55 -0700

    Delete FRONTEND directory

--- FRONTEND/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import errorsReducer from '../store/errorsSlice'
-
-export const store = configureStore({
-  reducer: {
-    errors: errorsReducer
-  }
-})
